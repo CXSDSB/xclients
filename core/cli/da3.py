8b841cefab2174198fefb9c54dcc791f9aa5393b
Matt Hyatt <mhyatt000@gmail.com>
2025-11-25 02:09:00 -0500

    da3 client

--- core/cli/da3.py
+++ core/cli/da3.py
@@ -1,30 +1,59 @@
-import glob
-import os
+from __future__ import annotations
 
-import torch
-from depth_anything_3.api import DepthAnything3
+import logging
+from dataclasses import dataclass
+
+import cv2
+import jax
+import numpy as np
+import tyro
+from rich import print
+from webpolicy.client import Client
 
 
-def main():
-    device = torch.device("cuda")
-    model = DepthAnything3.from_pretrained("depth-anything/DA3NESTED-GIANT-LARGE")
-    model = model.to(device=device)
-    example_path = "assets/examples/SOH"
-    images = sorted(glob.glob(os.path.join(example_path, "*.png")))
-    prediction = model.inference(
-        images,
+def spec(tree: dict[str, jax.Array] | jax.Array):
+    return jax.tree.map(
+        lambda x: (x.shape, x.dtype) if isinstance(x, jax.Array | np.ndarray) else type(x), tree
     )
-    # prediction.processed_images : [N, H, W, 3] uint8   array
-    print(prediction.processed_images.shape)
-    # prediction.depth            : [N, H, W]    float32 array
-    print(prediction.depth.shape)
-    # prediction.conf             : [N, H, W]    float32 array
-    print(prediction.conf.shape)
-    # prediction.extrinsics       : [N, 3, 4]    float32 array # opencv w2c or colmap format
-    print(prediction.extrinsics.shape)
-    # prediction.intrinsics       : [N, 3, 3]    float32 array
-    print(prediction.intrinsics.shape)
+
+
+@dataclass
+class Config:
+    host: str = "127.0.0.1"
+    port: int = 8080
+    show: bool = False
+
+
+def main(cfg: Config) -> None:
+    client = Client(cfg.host, cfg.port)
+    cap = cv2.VideoCapture(0)
+
+    logging.info("Displaying frames from camera 0; press 'q' to quit.")
+    while True:
+        ret, frame = cap.read()
+        print(frame.shape)
+        payload = {"image": [frame]}
+        out = client.step(payload)
+        if not out:
+            logging.error("Failed to read frame from camera 0")
+            continue
+
+        print(spec(out))
+        d = out["depth"][0]
+        d = ((d - np.min(d)) / (np.max(d) - np.min(d)) * 255.0).astype(np.uint8)
+        d = 255 - d
+
+        print(out["extrinsics"])
+        print(out["intrinsics"])
+
+        if cfg.show:
+            cv2.imshow("Camera 0", d)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO)
+    main(tyro.cli(Config))
